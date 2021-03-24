2e548ba8a2edf1f7f4fb9b8e048ba674ea7e0f5f
Luiz Henrique Souza <henryque.souz4@gmail.com>
2021-03-24 11:42:02 -0400

    feat: use typeorm repository in routes and services

--- src/database/migrations/1616596834585-CreateAppointments.ts
+++ src/database/migrations/1616596834585-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1616596834585
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
