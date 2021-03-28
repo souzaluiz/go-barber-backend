8fa283296bed2091f2d0aa6a0f99d12e65108a32
Luiz Henrique Souza <henryque.souz4@gmail.com>
2021-03-28 11:31:11 -0400

    feat: add relations in Appointment

--- src/database/migrations/1616596834585-CreateAppointments.ts
+++ src/database/migrations/1616596834585-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1616596834585
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
