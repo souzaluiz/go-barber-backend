37ac56bb3c8e1f7b0b994368f2af59c7f26606ce
Luiz Henrique Souza <henryque.souz4@gmail.com>
2021-04-05 17:37:50 -0400

    feat: add global error handler

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -10,19 +10,15 @@ const usersRoutes = Router();
 const upload = multer(uploadConfig);
 
 usersRoutes.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
+  const createUser = new CreateUserService();
 
-    const user = await createUser.execute({ name, email, password });
+  const user = await createUser.execute({ name, email, password });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json(user);
-  } catch (error) {
-    return response.status(400).json({ error: error.message });
-  }
+  return response.json(user);
 });
 
 usersRoutes.patch(
@@ -30,20 +26,16 @@ usersRoutes.patch(
   ensureAuthenticaded,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const updateUserAvatarService = new UpdateUserAvatarService();
+    const updateUserAvatarService = new UpdateUserAvatarService();
 
-      const user = await updateUserAvatarService.execute({
-        user_id: request.user.id,
-        avatarFilename: request.file.filename,
-      });
+    const user = await updateUserAvatarService.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
 
-      delete user.password;
+    delete user.password;
 
-      return response.json(user);
-    } catch (error) {
-      return response.status(400).json({ error: error.message });
-    }
+    return response.json(user);
   },
 );
 
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import routes from './routes';
+
+import AppError from './errors/AppError';
 
 import './database';
 import uploadConfig from './config/upload';
@@ -8,5 +11,20 @@ const app = express();
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      return response
+        .status(error.statusCode)
+        .json({ status: 'error', message: error.message });
+    }
+
+    console.error(error);
+
+    return response
+      .status(500)
+      .json({ status: 'error', message: 'Internal server error.' });
+  },
+);
 
 app.listen(process.env.PORT || 3333);
